fbf8f3d79964fc2c07de90e2ae2b2d7f6f1cde50
Clemens Seiler <cseiler@htl-steyr.ac.at>
2025-11-24 13:30:01 +0100

    Added File Cseiler and called Name auf Teammate with Method tell

--- ProjektMain.java
+++ ProjektMain.java
@@ -3,7 +3,10 @@ class ProjektMain{
    public static void main(String[] args){
 
        Fschmidb fschmidb = new Fschmidb();
+       Cseiler cseiler = new cseiler;
 
-       fschmidb.tell;
+       cseiler.tell();
+       fschmidb.tell();
+
    }
 }
