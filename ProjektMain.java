0ac1973fc829b20089262b5362ad1f6a448fce7f
Fabian <fschmidb@htl-steyr.ac.at>
2025-11-24 13:35:45 +0100

    Added code

--- ProjektMain.java
+++ ProjektMain.java
@@ -3,6 +3,7 @@ class ProjektMain{
    public static void main(String[] args){
 
        Fschmidb fschmidb = new Fschmidb();
+       Fschmidb fschmidb1 = new Fschmidb();
 
        fschmidb.tell;
    }
